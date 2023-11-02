d028f9f7a22d58fef7a7bc4204e4c2903bb625a2
prinztek <121484657+prinztek@users.noreply.github.com>
2023-11-02 19:09:41 +0800

    Updated Player Class to work with modified Person Class

--- src/Player.java
+++ src/Player.java
@@ -5,10 +5,10 @@ public class Player {
 		int year = 2023;
 		System.out.println("Hello World! It's " + year + "!");
 		
-		Person tom = new Person("Tom", 225.3f);
+		Person tom = new Person("Tom", 225.3f, 6.0f);
 		tom.Walks();
 		
-		Person mike = new Person("Mike", 180.9f);
+		Person mike = new Person("Mike", 180.9f, 6.5f);
 		mike.Walks();
 		
 	}
